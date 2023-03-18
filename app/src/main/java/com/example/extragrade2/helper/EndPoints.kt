13102662364dd341148d0fdbd0a1bf4ada504746
Arnaud <arnaud.a.Durand@gmail.com>
2023-03-18 19:17:36 +0200

    v4 - utility and helper class done

---
+++ app/src/main/java/com/example/extragrade2/helper/EndPoints.kt
@@ -0,0 +1,17 @@
+package com.example.extragrade2.helper
+
+class EndPoints {
+
+    companion object {
+
+        //Base URL        const val BASE_URL =     "https://api.getgeoapi.com/api/v2/currency/"
+
+        //API KEY - Go to geo currency converter website, obtain an                                       API key and paste it between " "
+
+        const val API_KEY = "3abab267dfd5c8e69e8ff81e93b65047884f9940"
+
+        //COVERT URL
+        const val CONVERT_URL = "convert"
+
+    }
+}
