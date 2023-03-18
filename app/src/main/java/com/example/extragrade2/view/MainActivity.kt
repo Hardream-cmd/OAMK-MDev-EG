96b9aced5b52d16ba805f97fb44337792e981895
Arnaud <arnaud.a.Durand@gmail.com>
2023-03-18 17:34:14 +0200

    v2-until font file wkg

--- app/src/main/java/com/example/extragrade2/view/MainActivity.kt
+++ app/src/main/java/com/example/extragrade2/view/MainActivity.kt
@@ -6,6 +6,9 @@ import com.example.extragrade2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //set the activity theme :
+        setTheme(R.style.AppTheme_NoActionBar)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
